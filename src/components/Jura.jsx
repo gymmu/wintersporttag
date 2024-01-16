026c9634d43d803483c83cb417c2010d76a7005e
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-01-16 15:39:23 +0100

    Jurawanderung übersicht.

--- src/components/Jura.jsx
+++ src/components/Jura.jsx
@@ -1,7 +1,58 @@
+import "./jura.css"
+
 export default function Jura() {
     return (
-    <>
-            <h2>Jura Wanderung</h2>
-    </>
+        <>
+            <div className="container jura-container">
+                <h2>Jurawanderung</h2>
+                <div className="jura-entry">
+                    Als Alternative zum Skifahren bieten wir eine wunderschöne Wanderung im Jura an. Die Anreise erfolgt individuell mit dem öffentlichen Verkehr bis nach Gelterkinden. Ab dort sind Sitzplätze im Postauto für Sie reserviert. Den Fahrplan finden Sie <a href="/wintersporttag/jura/times">hier</a>.
+                </div>
+
+                <div className="jura-entry">
+                    <h3>Sportlicher Teil</h3>
+                    Diese Wanderung wird als geführte Tour durchgeführt und findet ausschliesslich in geschlossener Gruppe statt. Es ist strengstens untersagt, sich von der Gruppe zu entfernen.
+                </div>
+
+
+                <div className="jura-entry">
+                    <h3>Kosten</h3>
+                    Die Reiseroute befindet sich im Gebiet des Tarifverbundes Nordwestschweiz. Die GA,
+                    Halbtax und U-Abos sind gültig.
+                    Die Fahrkarten für die Hinfahrt nach Zeiningen und für die Rückfahrt von Oltingen sind
+                    Sache der Teilnehmenden.
+                </div>
+
+                <div className="jura-entry">
+                    <h3>Ausrüstung</h3>
+                    Warme Kleider, Wanderschuhe oder gute Winterschuhe (keine Turnschuhe!). Die
+                    Wanderung führt je nach Witterung durch Schnee und Eis.
+                </div>
+
+                <div className="jura-entry">
+                    <h3>Verplegung</h3>
+                    Auf eigene Kosten (in Oltingen gibt es ein Restaurant).
+                </div>
+
+                <div className="jura-entry">
+                    <h3>Rauchen, Kiffen Alkohol</h3>
+                    Kiffen und der Konsum von Alkohol sind während des ganzen Wintersporttags (Reise
+                    inbegriffen) verboten.
+                </div>
+
+                <div className="jura-entry">
+                    <h3>Begleitung Lehrpersonen</h3>
+                    Die begleitenden Lehrpersonen sind zuständig für die Information der TeilnehmerInnen vor
+                    Ort. Die Präsenzkontrollen erfolgen auf der Hin- und der Rückfahrt: in Zeglingen vor Beginn
+                    der Wanderung und in Oltingen am Ende der Wanderung.
+                </div>
+
+                <div className="jura-entry">
+                    <h3>Schulbeginn</h3>
+                    Der Unterricht am Tag nach dem Wintersporttag beginnt in der 2. Lektion.
+                </div>
+
+            </div>
+        </>
     )
 }
