7c7aaa5d9697c81943c646e76b513972206f92c8
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-01-16 17:43:54 +0100

    Fixed typo.

--- src/components/Emergency.jsx
+++ src/components/Emergency.jsx
@@ -7,7 +7,7 @@ export default function Emergency() {
 
                 <div className="spacer"></div>
 
-                <h2>Zu verpasst?</h2>
+                <h2>Zug verpasst?</h2>
                 Sollten Sie es nicht rechtzeitig zum Zug schaffen, informieren Sie bitte umgehend die Lehrpersonen Cedric Geissmann und Walter Käch, damit eine betreute Rückreise organisiert werden kann. Es ist äußerst wichtig, den Zug nicht zu verpassen.
             </div>
         </>
