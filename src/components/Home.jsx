1d3aaadca6fd5547e9fbccd44f5624f48f3b9dc5
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-01-16 06:49:41 +0100

    Better texts.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,155 @@
+import "./home.css"
+
 export default function Home() {
-  return (
-    <>
-      <h2>Home</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis error
-        itaque exercitationem commodi consequatur hic reprehenderit odit fugit
-        at quis ea doloribus facere quisquam aut, aperiam consectetur, in nemo
-        temporibus. Illum placeat explicabo, eum natus nisi tenetur. Natus
-        facilis rerum nisi ut. Eum odio inventore voluptatibus similique
-        quibusdam recusandae culpa.
-      </p>
-    </>
-  );
+    return (
+        <>
+            <h2>Angebote vor Ort</h2>
+
+            <div className="activity">
+                <div className="activity-header">
+                    Wandern Flumserberg Tannenboden - Seeben
+                </div>
+                <div className="activity-body">
+                    <div className="activity-description">
+                        Begleite uns auf einer atemberaubenden Wanderung entlang einer malerischen Strecke von insgesamt 8 Kilometern - hin und zurück. Geniesse die unberührte Natur und erlebe die Schönheit der Landschaft hautnah. Doch das ist noch nicht alles! Auf halber Strecke erwartet dich das Restaurant Seeben Bad, ein wahres Juwel mit einer wunderschönen, ruhigen Sonnenterrasse. Hier kannst du dich entspannen und die Sonne auf deiner Haut spüren, während du den Blick über den glitzernden See schweifen lässt. Lass dich von der herzlichen Atmosphäre und dem köstlichen Essen verwöhnen, bevor du gestärkt den Rückweg antrittst. Diese Wanderung ist ein absolutes Muss für Naturliebhaber und Geniesser gleichermassen. Komm mit und erlebe unvergessliche Momente inmitten der Natur!
+                    </div>
+                    <div className="activity-teacher">
+                        M. Stöcklin
+                    </div>
+                    <div className="activity-meeting">
+                        11:15 Uhr Tannenboden (im Anschluss an Gondelfahrt)
+                    </div>
+                </div>
+            </div>
+
+            <div className="activity">
+                <div className="activity-header">
+                    Wandern Flumserberg Maschgenkamm – Panüöl - Prodalp
+                </div>
+                <div className="activity-body">
+                    <div className="activity-description">
+                        Begleiten Sie uns auf einer atemberaubenden Wanderung entlang des 8 km langen Panoramawegs, der spektakuläre Ausblicke bietet. Genießen Sie die malerische Landschaft und lassen Sie sich von der Schönheit der Natur verzaubern. Nach der Wanderung erwartet Sie ein weiteres Highlight: eine entspannte Talfahrt mit der Seilbahn. Lehnen Sie sich zurück und lassen Sie sich von der beeindruckenden Aussicht auf dem Weg hinunter ins Tal begeistern.
+                    </div>
+                    <div className="activity-teacher">
+                        K. Leiva, G. Schaller, B. Jäggi
+                    </div>
+                    <div className="activity-meeting">
+                        11:15 Uhr Tannenheim (im Anschluss an Busfahrt)
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="activity">
+                <div className="activity-header">
+                    Wandern Flumserberg Maschgenkamm – Panüöl - Prodalp
+                </div>
+                <div className="activity-body">
+                    <div className="activity-description">
+                        Erleben Sie eine atemberaubende Bergfahrt mit der Seilbahn und begleiten Sie anschliessend unsere Gruppe auf einer malerischen Panoramawanderung zurück nach Prodalp oder Tannenheim. Tauchen Sie ein in die Schönheit der Natur und geniessen Sie unvergessliche Ausblicke auf die umliegende Berglandschaft. Kommen Sie mit uns auf dieses unvergessliche Abenteuer und lassen Sie sich von der Magie der Berge verzaubern.
+                    </div>
+                    <div className="activity-teacher">
+                        A. Kessler, W. Käch
+                    </div>
+                    <div className="activity-meeting">
+                        11:30h Bergstation Maschgenkamm (im Anschluss an Bergfahrt)
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="activity">
+                <div className="activity-header">
+                    Skifahren mit fortgeschrittenen Anfänger:innen
+                </div>
+                <div className="activity-body">
+                    <div className="activity-description">
+                        Treffpunkt Tannenboden; Betreuung 11:00h bis 13:00
+                    </div>
+                    <div className="activity-teacher">
+                        S. Kulka
+                    </div>
+                    <div className="activity-meeting">
+                        11:15h Tannenboden (im Anschluss an Gondelfahrt)
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="activity">
+                <div className="activity-header">
+                    Skifahren mit Fortgeschrittenen
+                </div>
+                <div className="activity-body">
+                    <div className="activity-description">
+                        Treffpunkt Maschgenkamm; Betreuung am Anfang und dann nach Absprache
+                    </div>
+                    <div className="activity-teacher">
+                        R. Morf
+                    </div>
+                    <div className="activity-meeting">
+                        11:30h Bergstation Maschgenkamm (im Anschluss an Bergfahrt)
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="activity">
+                <div className="activity-header">
+                    Snowboard mit fortgeschrittenen Anfänger:innen
+                </div>
+                <div className="activity-body">
+                    <div className="activity-description">
+                        Treffpunkt Tannenboden; Betreuung am Anfang und dann nach Absprache
+                    </div>
+                    <div className="activity-teacher">
+                        J. Gees
+                    </div>
+                    <div className="activity-meeting">
+                        11:15h Tannenboden (im Anschluss an Gondelfahrt)
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="activity">
+                <div className="activity-header">
+                    Snowboard mit Fortgeschrittenen
+                </div>
+                <div className="activity-body">
+                    <div className="activity-description">
+                        Treffpunkt Maschgenkamm; Betreuung am Anfang und dann nach Absprache
+                    </div>
+                    <div className="activity-teacher">
+                        C. Geissmann
+                    </div>
+                    <div className="activity-meeting">
+                        11:30h Bergstation Maschgenkamm (im Anschluss an Bergfahrt)
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <div className="activity">
+                <div className="activity-header">
+                    Langlauf
+                </div>
+                <div className="activity-body">
+                    <div className="activity-description">
+                        Betreuung nach Absprache
+                    </div>
+                    <div className="activity-teacher">
+                        A. Marbacher, B. Grossmann, A. Kaech, S. Schraudolf
+                    </div>
+                    <div className="activity-meeting">
+                        Nach Absprache (Interessierte melden sich bei den LP)
+                    </div>
+                </div>
+            </div>
+
+
+        </>
+    );
 }
