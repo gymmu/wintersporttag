85be4b61249e7395c75ec60b0357ebe1991a01fa
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-01-18 07:54:46 +0100

    Small fixes in style.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,30 +7,48 @@ import Rules from "./Rules"
 import Emergency from "./Emergency";
 import JuraTimes from "./JuraTimes";
 import JuraEmergency from "./JuraEmergency"
+import {useLayoutEffect} from "preact/hooks";
 import { Router } from "preact-router";
+import { useLocation } from "wouter-preact";
+
 import "./main.css";
 
 function Main({ children }) {
-  return <main>{children}</main>;
+    return <main>{children}</main>;
+}
+
+
+function Wrapper({ children }) {
+    const location = useLocation()
+    useLayoutEffect(() => {
+        window.scrollTo(0, 0)
+    }, [location])
+    return (
+        <>
+            {children}
+        </>
+    )
 }
 
 export function App() {
-  return (
-    <>
-      <Header />
-      <Main>
-        <Router>
-          <Home default path="/wintersporttag/" />
-          <Times path="/wintersporttag/times" />
-          <Rules path="/wintersporttag/rules" />
-          <Emergency path="/wintersporttag/emergency" />
+    return (
+        <>
+            <Header />
+            <Main>
+                <Wrapper>
+                    <Router>
+                        <Home default path="/wintersporttag/" />
+                        <Times path="/wintersporttag/times" />
+                        <Rules path="/wintersporttag/rules" />
+                        <Emergency path="/wintersporttag/emergency" />
 
-          <Jura path="/wintersporttag/jura" />
-          <JuraTimes path="/wintersporttag/jura/times" />
-          <JuraEmergency path="/wintersporttag/jura/emergency" />
-        </Router>
-      </Main>
-      <Sidebar />
-    </>
-  );
+                        <Jura path="/wintersporttag/jura" />
+                        <JuraTimes path="/wintersporttag/jura/times" />
+                        <JuraEmergency path="/wintersporttag/jura/emergency" />
+                    </Router>
+                </Wrapper>
+            </Main>
+            <Sidebar />
+        </>
+    );
 }
--- src/components/GymLogo.jsx
+++ src/components/GymLogo.jsx
@@ -3,7 +3,7 @@ import gymLogo from '../assets/logo-gym-muttenz.png'
 export default function GymLogo( {style}) {
     return <div style={style}>
         <a href="https://www.gym-muttenz.ch">
-        <img style={{ height: '100%' }} src={gymLogo} class="logo" alt="Gym Muttenz logo" />
+        <img style={{ height: '100%', padding: '0.25rem' }} src={gymLogo} class="logo" alt="Gym Muttenz logo" />
         </a>
     </div>
 }
